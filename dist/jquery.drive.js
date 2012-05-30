0e2eff01b59ff318d73eb2e275a4ebb637febc0d
Laurent Fortin <laurent.fortin@gmail.com>
2012-05-29 23:59:32 -0400

    new version 1.2.1

--- dist/jquery.drive.js
+++ dist/jquery.drive.js
@@ -1,7 +1,7 @@
-// Drive jQuery plugin v1.2.0
+// Drive jQuery plugin v1.2.1
 // http://github.com/lfortin/drive-jquery-plugin#readme
 
-// Copyright (c) 2009-2011 Laurent Fortin
+// Copyright (c) 2009-2012 Laurent Fortin
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -64,7 +64,7 @@ try {
 	/**** Drive class methods ****/
 
 	$.extend(internal.Drive.prototype, {
-		version: '1.2.0',
+		version: '1.2.1',
 
 		// initialize all config
 		init: function(cfg) {
